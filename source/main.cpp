a632e8dbb7e0e44c17d6f4c1ca1f6b2681d53b29
Alex Beals <alexcbeals@gmail.com>
2020-11-28 01:09:17 -0800

    Remove a bunch of nonsense

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,7 @@
 #include <nds.h>
 #include <nds/arm9/input.h>
 #include <gl2d.h>
+#include <nf_lib.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
@@ -70,62 +71,10 @@ int main(int argc, char** argv){
 		renderBackground();
 
 		renderPlayer(&player);
-		// checkHitbox(&player, &pipes);
-		// renderPipes(&pipes);
 		glEnd2D();
 		glFlush(0);
 		swiWaitForVBlank();
 	}
-
-	// touchPosition touch;
-
-	// //Set Upper Screen
-	// videoSetMode(MODE_5_2D);
-    // vramSetBankA(VRAM_A_MAIN_BG_0x06000000);
-	// // videoSetMode(MODE_0_2D);
-	// // vramSetBankA(VRAM_A_MAIN_SPRITE);
-	// oamInit(&oamMain, SpriteMapping_1D_128, false);
-
-	// //Set Bottom Screen
-	// videoSetModeSub(MODE_0_2D);
-	// vramSetBankD(VRAM_D_SUB_SPRITE);
-
-	// int bg3 = bgInit(3, BgType_Bmp8, BgSize_B8_256x256, 0,0);
-
-	// dmaCopy(skeldBitmap, bgGetGfxPtr(bg3), 256*256);
-	// dmaCopy(skeldPal, BG_PALETTE, 256*2);
-	
-	// oamInit(&oamSub, SpriteMapping_1D_128, false);
-
-	// spriteKeyDown.gfx = oamAllocateGfx(&oamMain, SpriteSize_16x16, SpriteColorFormat_Bmp);
-	// spriteTouchDown.gfx = oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_Bmp);
-
-	// while(1){
-
-	// 	scanKeys();
-	// 	int key = keysHeld();
-
-	// 	if(key & KEY_UP)
-	// 		spriteKeyDown.y -= speed;
-	// 	if(key & KEY_DOWN)
-	// 		spriteKeyDown.y += speed;
-	// 	if(key & KEY_RIGHT)
-	// 		spriteKeyDown.x += speed;
-	// 	if(key & KEY_LEFT)
-	// 			spriteKeyDown.x -= speed;
-	// 	if(key & KEY_TOUCH) {
-	// 		touchRead(&touch);
-	// 			spriteTouchDown.x = touch.px;
-	// 			spriteTouchDown.y = touch.py;
-	// 	}
-
-	// 	createSquare(spriteKeyDown, &oamMain, 1);
-	// 	createSquare(spriteTouchDown, &oamSub, 2);
-
-	// 	swiWaitForVBlank(); // clean the screen
-	// 	oamUpdate(&oamMain); // (sub) updates the oam before so VBlank can update the graphics on screen
-	// 	oamUpdate(&oamSub); // (sub) updates the oam before so VBlank can update the graphics on screen
-	// }
 
 	return 0;
 }
